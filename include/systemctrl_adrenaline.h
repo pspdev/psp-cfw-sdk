97e2b3e983ac807fa9a677d1e5654cac40686351
GrayJack <gr41.j4ck@gmail.com>
2026-01-21 12:58:17 -0300

    refactor(epi): Remove massive includes in `systemctrl_adrenaline.h`

--- include/systemctrl_adrenaline.h
+++ include/systemctrl_adrenaline.h
@@ -1,54 +1,7 @@
 #ifndef _SYSTEMCTRL_EPI_H_
 #define _SYSTEMCTRL_EPI_H_
 
-#include <pspsdk.h>
-#include <pspkernel.h>
-
-#include <psperror.h>
-
-#include <kubridge.h>
-#include <systemctrl.h>
-#include <systemctrl_se.h>
-#include <psperror.h>
-
-#include <pspexception.h>
-#include <pspsysevent.h>
-#include <psputilsforkernel.h>
-#include <pspsysmem.h>
-#include <psploadexec.h>
-#include <pspthreadman_kernel.h>
-
-#include <pspusb.h>
-#include <pspusbstor.h>
-#include <pspumd.h>
-#include <psprtc.h>
-#include <pspreg.h>
-#include <pspctrl.h>
-#include <psppower.h>
-#include <pspaudio.h>
-#include <pspdisplay.h>
-#include <pspdisplay_kernel.h>
-#include <psputility.h>
-#include <pspcrypt.h>
-
-#include <rebootexconfig.h>
-#include <cfwmacros.h>
-
-// If COMMON_H_SYSCLIB_USER is defined, it includes the sysclib_user.h definitions
-// And other other headers from STD C must be manually included if necessary
-#ifdef COMMON_H_SYSCLIB_USER
-#include <sysclib_user.h>
-#include <stdarg.h>
-#else
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <ctype.h>
-#include <malloc.h>
-#endif
-
-#include <pspextratypes.h>
+#include <psploadcore.h>
 
 enum InfernoCacheConf {
 	CACHE_CONFIG_LRU,

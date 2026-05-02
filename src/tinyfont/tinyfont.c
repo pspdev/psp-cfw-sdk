3e1ac03bdf39298fbaab83d5523d03e5e4b7173b
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-05-02 13:34:09 +0200

    improvements to tinyfont

--- src/tinyfont/tinyfont.c
+++ src/tinyfont/tinyfont.c
@@ -7,8 +7,82 @@
 #define PSP_LINE_SIZE 512
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 272
+#define FRAMEBUFFER_SIZE (PSP_LINE_SIZE*SCREEN_HEIGHT*4)
 
-void tinyFontPrintText(u32* frame, u8* font, int x, int y, const char* text, u32 color, TinyFontState* state){
+static void* g_vram_base = (void*) (0x40000000 | 0x04000000);
+static int cur_fb = 0;
+
+static void* getVramDrawBuffer()
+{
+    void* vram = (void*) g_vram_base;
+    if (cur_fb == 0) vram += FRAMEBUFFER_SIZE;
+    return vram;
+}
+
+static unsigned int next_pow2(unsigned int v)
+{
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    return v+1;
+}
+
+void tinyFontSwapBuffers(){
+    cur_fb ^= 1;
+}
+
+void tinyFontPrintTextScreen(
+    u8* font,
+    int x,
+    int y,
+    const char* text,
+    u32 color,
+    TinyFontState* state
+) {
+    tinyFontPrintTextBuffer(getVramDrawBuffer(), SCREEN_WIDTH, SCREEN_HEIGHT, PSP_LINE_SIZE, font, x, y, text, color, state);
+}
+
+void tinyFontPrintTextScreenBuf(
+    void* framebuf,
+    u8* font,
+    int x,
+    int y,
+    const char* text,
+    u32 color,
+    TinyFontState* state
+){
+    tinyFontPrintTextBuffer(framebuf, SCREEN_WIDTH, SCREEN_HEIGHT, PSP_LINE_SIZE, font, x, y, text, color, state);
+}
+
+void tinyFontPrintTextImage(
+    void* image,
+    int img_w,
+    int img_h,
+    u8* font,
+    int x,
+    int y,
+    const char* text,
+    u32 color,
+    TinyFontState* state
+){
+    tinyFontPrintTextBuffer(image, img_w, img_h, next_pow2(img_w), font, x, y, text, color, state);
+}
+
+void tinyFontPrintTextBuffer(
+    u32* buffer,
+    int buf_w,
+    int buf_h,
+    int pow2_w,
+    u8* font,
+    int x,
+    int y,
+    const char* text,
+    u32 color,
+    TinyFontState* state
+){
     int c, i, j, l;
     u8 *font_ptr;
     u32 *vram_ptr;
@@ -42,9 +116,9 @@ void tinyFontPrintText(u32* frame, u8* font, int x, int y, const char* text, u32
     }
 
     for (c = 0; c < strlen(text); c++) {
-        if (x < 0 || x + 8 > SCREEN_WIDTH || y < 0 || y + 8 > SCREEN_HEIGHT) break;
+        if (x < 0 || x + 8 > buf_w || y < 0 || y + 8 > buf_h) break;
         char ch = text[c];
-        vram = frame + x + y * PSP_LINE_SIZE;
+        vram = buffer + x + y * pow2_w;
         
         font_ptr = &font[ (int)ch * 8];
         for (i = l = 0; i < 8; i++, l += 8, font_ptr++) {
@@ -61,14 +135,14 @@ void tinyFontPrintText(u32* frame, u8* font, int x, int y, const char* text, u32
                     if (vram_ptr[-1] != color) vram_ptr[-1] = bgcolor;
                     vram_ptr[1] = bgcolor;
                     // vertical shadow
-                    u32* prev_pixel = (u32*)((u32)vram_ptr - (4*PSP_LINE_SIZE));
-                    u32* post_pixel = (u32*)((u32)vram_ptr + (4*PSP_LINE_SIZE));
+                    u32* prev_pixel = (u32*)((u32)vram_ptr - (4*pow2_w));
+                    u32* post_pixel = (u32*)((u32)vram_ptr + (4*pow2_w));
                     if (*prev_pixel != color) *prev_pixel = bgcolor;
                     *post_pixel = bgcolor;
                 }
                 vram_ptr++;
             }
-            vram += PSP_LINE_SIZE;
+            vram += pow2_w;
         }
         x += 8;
     }
--- src/tinyfont/tinyfont.h
+++ src/tinyfont/tinyfont.h
@@ -15,6 +15,36 @@ typedef struct TinyFontState{
     u8 sd; // current shadow direction (increase/decrease), for glow
 } TinyFontState;
 
-void tinyFontPrintText(u32* frame, u8* font, int x, int y, const char* text, u32 color, TinyFontState* state);
+extern u8 msx[]; // built-in font
+
+/**
+ * Swap hardcoded draw/display buffers. Use this if you are not specifying a framebuffer (i.e. tinyFontPrintTextScreen).
+ */
+void tinyFontSwapBuffers();
+
+
+/**
+ * Print a string onto a draw buffer
+ * 
+ * @param buffer image buffer or framebuffer
+ * @param buf_w buffer width
+ * @param buf_h buffer height
+ * @param pow2_w next power of 2 after buffer width
+ * @param font bitmap font, can use built-in msx font
+ * @param x coordinate relative to frame buffer
+ * @param y coordinate relative to frame buffer
+ * @param text string to print onto buffer
+ * @param color 32 bit ARGB format color
+ * @param state optional, used for effects (glow, scroll, etc) 
+ * 
+ */
+void tinyFontPrintTextBuffer(u32* buffer, int buf_w, int buf_h, int pow2_w, u8* font, int x, int y, const char* text, u32 color, TinyFontState* state);
+
+void tinyFontPrintTextImage(void* image, int img_w, int img_h, u8* font, int x, int y, const char* text, u32 color, TinyFontState* state);
+
+void tinyFontPrintTextScreen(u8* font, int x, int y, const char* text, u32 color, TinyFontState* state);
+
+void tinyFontPrintTextScreenBuf(void* framebuf, u8* font, int x, int y, const char* text, u32 color, TinyFontState* state);
+
 
 #endif

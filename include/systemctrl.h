0fb74c52caab20374058e830c185826131b7514e
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-05-12 22:04:49 +0200

    refactor psp models enum

--- include/systemctrl.h
+++ include/systemctrl.h
@@ -40,14 +40,17 @@
 
 // Different PSP models
 enum {
-    PSP_1000 = 0,   // 01g
-    PSP_2000 = 1,   // 02g
-    PSP_3000 = 2,   // 03g
-    PSP_4000 = 3,   // 04g
-    PSP_GO   = 4,   // 05g
-    PSP_7000 = 6,   // 07g
-    PSP_9000 = 8,   // 09g
-    PSP_11000 = 10, // 11g
+    PSP_1000   =  0,   // 01g
+    PSP_2000   =  1,   // 02g
+    PSP_3000   =  2,   // 03g
+    PSP_4000   =  3,   // 04g
+    PSP_5000   =  4,   // 05g
+    PSP_6000   =  5,   // 06g (the myth, the legend)
+    PSP_7000   =  6,   // 07g
+    PSP_9000   =  8,   // 09g
+    PSP_11000  = 10,   // 11g
+    PSP_GO     = PSP_5000,
+    PSP_STREET = PSP_11000,
 };
 
 enum BootLoadFlags {
